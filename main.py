af2a4c38cfb2154321165900a0caac3e46465a11
vinnie <vinayanayak99@gmail.com>
2023-01-21 14:23:44 +0530

    bug_fix commit 1

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print("12334")
     print("something")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
